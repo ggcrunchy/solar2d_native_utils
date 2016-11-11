9cd34759810b2692c0f52f5f6a8a82f41c6f7c05
Steven Johnson <steve@xibalbastudios.com>
2016-11-11 15:01:17 -0600

    Added pthread-based TLS for iOS (which has no compiler support for it, at least in the version I've been using)

---
+++ utils/Thread.cpp
@@ -0,0 +1,81 @@
+/*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+*/
+
+#include "utils/Thread.h"
+
+#if TARGET_OS_IOS
+	#include <functional>
+	#include <vector>
+	#include <pthread.h>
+
+	namespace TLSXS {
+		static std::vector<POD> * GetData (pthread_key_t key)
+		{
+			return static_cast<std::vector<POD> *>(pthread_getspecific(key));
+		}
+
+		static pthread_key_t tls_key;
+
+		size_t GetSlot (void)
+		{
+			static pthread_once_t tls_key_once = PTHREAD_ONCE_INIT;
+
+			pthread_once(&tls_key_once, []()
+			{
+				pthread_key_create(&tls_key, [](void * data)
+				{
+					if (data) delete static_cast<std::vector<POD> *>(data);
+				});
+
+				atexit([]() { pthread_key_delete(tls_key); });
+			});
+
+			auto vec = GetData(tls_key);
+
+			if (!vec)
+			{
+				vec = new std::vector<POD>;
+
+				pthread_setspecific(tls_key, vec);
+			}
+
+			POD pod;
+
+			memset(&pod, 0, sizeof(POD));
+
+			vec->push_back(pod);
+
+			return vec->size() - 1;
+		}
+
+		void GetItemInSlot (size_t slot, POD & pod)
+		{
+			pod = GetData(tls_key)->at(slot);
+		}
+
+		void SetItemInSlot (size_t slot, const POD & pod)
+		{
+			GetData(tls_key)->at(slot) = pod;
+		}
+	}
+#endif
--- utils/Thread.h
+++ utils/Thread.h
@@ -22,108 +22,102 @@
 */
 
 #pragma once
-#if 0
-#include "CoronaLua.h"
-#include <stdlib.h>
 
-#define TLS(prefix)	/* Thread-local storage key */									\
-					static pthread_key_t prefix##_TLS_Key;							\
-					/* Once-only initialisation of the key */						\
-					static pthread_once_t prefix##_TLS_key_once = PTHREAD_ONCE_INIT
-
-#define TLS_GETTER(prefix, type) type * prefix##_GetTLS (void) { return (type *)pthread_getspecific(prefix##_TLS_Key); }
-
-#define TLS_PUBLIC_GETTER(prefix, type) TLS(prefix);	\
-							TLS_GETTER(prefix, type)
-
-#define TLS_STATIC_GETTER(prefix, type) TLS(prefix);	\
-							static TLS_GETTER(prefix, type)
-
-#define TLS_INIT_NO_REGISTER(prefix, type)	pthread_once(&prefix##_TLS_key_once, []()					\
-											{															\
-												pthread_key_create(&prefix##_TLS_Key, NULL);			\
-												atexit([]() { pthread_key_delete(prefix##_TLS_Key); });	\
-											});															\
-																										\
-											lua_pushlightuserdata(L, &prefix##_TLS_Key);				\
-																										\
-											type * tls = (type *)lua_newuserdata(L, sizeof(type));		\
-																										\
-											pthread_setspecific(prefix##_TLS_Key, tls)
-
-#define TLS_INIT(prefix, type)	TLS_INIT_NO_REGISTER(prefix, type);	\
-								lua_settable(L, LUA_REGISTRYINDEX)
-
-#define TLS_MEMORY_INIT(prefix, type)	TLS_INIT(prefix, type);	\
-																\
-										new (tls) type();		\
-																\
-										tls->mL = L
-
-struct MemoryTLS {
-	lua_State * mL;	// Main Lua state for this 
-	int mIndex;	// Index of memory
-	int mRegistrySlot;	// Slot in registry, if used
-	int mStoreSlot;	// Slot in registry for table storage, if used
-
-	struct BookmarkDualTables {
-		MemoryTLS * mOwner;	// Memory TLS to repair
-
-		~BookmarkDualTables (void) { mOwner->UnloadTable(); }
-	};
-
-	struct BookmarkIndex {
-		MemoryTLS * mOwner;	// Memory TLS to repair
-		int mIndex;	// Saved index
-
-		~BookmarkIndex (void) { mOwner->mIndex = mIndex; }
-	};
-
-	BookmarkDualTables BindTable (void);
-	BookmarkIndex SavePosition (bool bRelocate = true);
-
-	size_t GetOldSize (int slot, void * ptr);
-
-	void * Add (int slot, size_t size);
-
-	int Begin (void);
-
-	void End (void);
-	void LoadTable (void);
-	void PrepDualTables (void);
-	void PrepMemory (int slot = 0);
-	void PrepRegistry (void);
-	void PushObject (int slot, void * ptr);
-	void Remove (int slot, void * ptr);
-	void UnloadTable (void);
-
-	MemoryTLS (void) : mL(NULL), mIndex(0), mRegistrySlot(LUA_NOREF), mStoreSlot(LUA_NOREF) {}
-};
-
-void FailAssertTLS (MemoryTLS * tls, const char * what);
-void * MallocTLS (MemoryTLS * tls, size_t size);
-void * CallocTLS (MemoryTLS * tls, size_t num, size_t size);
-void * ReallocTLS (MemoryTLS * tls, void * ptr, size_t size);
-void FreeTLS (MemoryTLS * tls, void * ptr);
-size_t GetSizeTLS (MemoryTLS * tls, void * ptr);
-bool EmitTLS (MemoryTLS * tls, void * ptr, bool bRemove = true);
-void PushTLS (MemoryTLS * tls, void * ptr, const char * type, bool bAsUserdata, bool bRemove = true);
-
-template<typename T, T * (*GET)(void)> struct MemoryFuncsTLS {
-	typedef MemoryFuncsTLS<T, GET> FuncsType;
-
-	static void FailAssert (const char * what) { FailAssertTLS(GET(), what); }
-	static void * Malloc (size_t size) { return MallocTLS(GET(), size); }
-	static void * Calloc (size_t num, size_t size) { return CallocTLS(GET(), num, size); }
-	static void * Realloc (void * ptr, size_t size) { return ReallocTLS(GET(), ptr, size); }
-	static void Free (void * ptr) { FreeTLS(GET(), ptr); }
-	static size_t GetSize (void * ptr) { return GetSizeTLS(GET(), ptr); }
-	static bool Emit (void * ptr) { return EmitTLS(GET(), ptr); }
-	static void Push (void * ptr, const char * type, bool as_userdata) { PushTLS(GET(), ptr, type, as_userdata); }
-};
-#endif
 #ifdef _WIN32
-	#define DECL_TLS __declspec(thread)
+	#define DECL_TLS(type) __declspec(thread) type
 #else
-	#define DECL_TLS __thread
+	#ifdef __APPLE__
+		#include "TargetCondtionals.h"
+	#endif
+
+	#if TARGET_OS_IOS
+		#include <type_traits>
+
+		namespace TLSXS {
+			union POD {
+				void * mP;
+				double mD;
+			};
+
+			template<typename T> struct Value {
+				POD mData;
+
+				T Get (void)
+				{
+					T value;
+
+					memcpy(&value, &mData, sizeof(T));
+
+					return value;
+				}
+
+				void Set (const T & value)
+				{
+					memcpy(&mData, &value, sizeof(T));
+				}
+
+				Value (void) {}
+
+				Value (const T & value)
+				{
+					Set(value);
+				}
+			};
+
+			size_t GetSlot (void);
+			void GetItemInSlot (size_t slot, POD & pod);
+			void SetItemInSlot (size_t slot, const POD & pod);
+
+			template<typename T> class TypeWithTLS {
+				size_t mSlot;
+
+			public:
+				TypeWithTLS (void)
+				{
+					mSlot = GetSlot();
+				}
+
+				TypeWithTLS (const T & value)
+				{
+					mSlot = GetSlot();
+
+					SetItemInSlot(mSlot, Value<T>(value).mData);
+				}
+
+				TypeWithTLS<T> & operator = (const T & value)
+				{
+					SetItemInSlot(mSlot, Value<T>(value).mData);
+
+					return *this;
+				}
+
+				operator T ()
+				{
+					Value<T> value;
+
+					GetItemInSlot(mSlot, value.mData);
+
+					return value.Get();
+				}
+
+				T operator -> () // workaround while testing on MSVC
+				{
+					if (std::is_pointer<T>::value)
+					{
+						Value<T> value;
+
+						GetItemInSlot(mSlot, value.mData);
+
+						return value.Get();
+					}
+
+					else return nullptr;
+				}
+			};
+		}
+
+		#define DECL_TLS(type) static TLSXS::TypeWithTLS<type>
+	#else
+		#define DECL_TLS(type) __thread type
+	#endif
 #endif
