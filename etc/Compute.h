a1526d22fc8855abcfb9ea206ba8a812a82ae847
Steven Johnson <steve@xibalbastudios.com>
2016-11-12 22:54:28 -0600

    Half-step to putting compute into own namespace (sort of in limbo at the moment)

--- etc/Compute.h
+++ etc/Compute.h
@@ -61,51 +61,51 @@
 	#endif
 #endif
 
-struct ComputeCaps {
-	typedef enum { eAMP, eCUDA, eMetal, eOpenCL, eRenderScript, eVULKAN } Flag;
+namespace ComputeXS {
+	struct Caps {
+		typedef enum { eAMP, eCUDA, eMetal, eOpenCL, eRenderScript, eVULKAN } Flag;
 
-	unsigned int mSupported;// Which backends are supported?
-	unsigned int mEnabled;	// Which backends are enabled? (i.e. supported and not shut down)
+		unsigned int mSupported;// Which backends are supported?
+		unsigned int mEnabled;	// Which backends are enabled? (i.e. supported and not shut down)
 
-#ifdef WANT_AMP
-	std::vector<concurrency::accelerator> mAccelerators;// List of AMP accelerators
-#endif
+	#ifdef WANT_AMP
+		std::vector<concurrency::accelerator> mAccelerators;// List of AMP accelerators
+	#endif
 
-#ifdef WANT_CUDA
-	struct DeviceInfoCUDA {
-		int mMajor, mMinor;
+	#ifdef WANT_CUDA
+		struct DeviceInfoCUDA {
+			int mMajor, mMinor;
+		};
+
+		std::vector<DeviceInfoCUDA> mDevicesCUDA;	// List of devices (index = device id) along with CUDA driver info
+	#endif
+
+		Caps (void) : mSupported(0), mEnabled(0) {}
 	};
 
-	std::vector<DeviceInfoCUDA> mDevicesCUDA;	// List of devices (index = device id) along with CUDA driver info
-#endif
+	bool CheckSupport (lua_State * L);
+	bool GetCaps (lua_State * L, Caps & caps);
+	void ShutDownBackend (Caps::Flag flag);
 
-	ComputeCaps (void) : mSupported(0), mEnabled(0) {}
-};
+	#ifdef WANT_CUDA
+	#include <cuda.h>
+	#endif
 
-bool CheckComputeSupport (lua_State * L);
-bool GetComputeCaps (lua_State * L, ComputeCaps & caps);
-void ShutDownBackend (ComputeCaps::Flag flag);
+	#ifdef WANT_METAL
+	//#include <metal.h> // apparently want to do this from a .mm file?
 
-#ifdef WANT_CUDA
-#include <cuda.h>
-#endif
+	//id<MTLDevice> GetMetalDevice ();
+	#endif
 
-#ifdef WANT_METAL
-//#include <metal.h> // apparently want to do this from a .mm file?
+	#ifdef WANT_OPENCL
+	// TODO: OpenCL!
+	#endif
 
-//id<MTLDevice> GetMetalDevice ();
-#endif
+	#ifdef WANT_RENDERSCRIPT
+		// TODO!
+	#endif
 
-#ifdef WANT_OPENCL
-// TODO: OpenCL!
-#endif
-
-#ifdef WANT_RENDERSCRIPT
-	// TODO!
-#endif
-
-#ifdef WANT_VULKAN
-	// TODO!
-#endif
-
-#endif
+	#ifdef WANT_VULKAN
+		// TODO!
+	#endif
+}
