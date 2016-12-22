3a13d6678d13f6f7cb596fc5a94546a75c23cb11
Steven Johnson <steve@xibalbastudios.com>
2016-12-21 18:50:06 -0600

    SIMD formatting
    Merged Android and non-Accelerate blocks together

--- utils/SIMD.cpp
+++ utils/SIMD.cpp
@@ -145,153 +145,149 @@ namespace SimdXS {
 
 		namespace ns_f2u8 = Neon;
         namespace ns_pv = Neon::PackedVector;*/
-	#if _WIN32
-		namespace ns_f2u8 = DirectX;
-        namespace ns_pv = DirectX::PackedVector;
-    #else
-        namespace ns_f2u8 = XMath;
-        namespace  ns_pv = ns_f2u8;
-	#endif
+#ifdef _WIN32
+	namespace ns_f2u8 = DirectX;
+    namespace ns_pv = DirectX::PackedVector;
+#else
+    namespace ns_f2u8 = XMath;
+    namespace  ns_pv = ns_f2u8;
+#endif
 
-	#ifdef HAS_ACCELERATE
-		template<bool dummy = false> static void AuxFloatsToUnorm8s (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool bNoTile)
-		{
-			vImage_Buffer src, dst;
+#ifdef HAS_ACCELERATE
+	template<bool dummy = false> static void AuxFloatsToUnorm8s (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool bNoTile)
+	{
+		vImage_Buffer src, dst;
           
-			src.data = (void *)pfloats;
-			dst.data = (void *)u8;
-			src.width = dst.width = dst.rowBytes = n;
-			src.height = dst.height = 1;
-			src.rowBytes = n * sizeof(float);
+		src.data = (void *)pfloats;
+		dst.data = (void *)u8;
+		src.width = dst.width = dst.rowBytes = n;
+		src.height = dst.height = 1;
+		src.rowBytes = n * sizeof(float);
                 
-			vImageConvert_PlanarFtoPlanar8(&src, &dst, 1.0f, 0.0f, bNoTile ? kvImageDoNotTile : 0);
-		}
-	#else
-		template<bool is_android = PlatformXS::is_android> static void AuxFloatsToUnorm8s (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool)
+		vImageConvert_PlanarFtoPlanar8(&src, &dst, 1.0f, 0.0f, bNoTile ? kvImageDoNotTile : 0);
+	}
+#else
+	template<bool is_android = PlatformXS::is_android> static void AuxFloatsToUnorm8s (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool)
+	{
+		void * ptr = const_cast<float *>(pfloats);
+
+		MemoryXS::Align(16U, n * sizeof(float), ptr);
+
+		ns_pv::XMUBYTEN4 out;
+
+		// Peel off any leading floats.
+		if (pfloats != ptr)
 		{
-			void * ptr = const_cast<float *>(pfloats);
+			size_t extra = static_cast<const float *>(ptr) - pfloats;
+			ns_f2u8::XMVECTORF32 padding = { 0 };
 
-			MemoryXS::Align(16U, n * sizeof(float), ptr);
+			for (size_t i = 0; i < extra; ++i, --n) padding.f[4U - extra + i] = pfloats[i];
 
-			ns_pv::XMUBYTEN4 out;
+			ns_pv::XMStoreUByteN4(&out, padding.v);
 
-			// Peel off any leading floats.
-			if (pfloats != ptr)
-			{
-				size_t extra = static_cast<const float *>(ptr) - pfloats;
-				ns_f2u8::XMVECTORF32 padding = { 0 };
+			memcpy(u8, reinterpret_cast<unsigned char *>(&out) + 4U - extra, extra);
 
-				for (size_t i = 0; i < extra; ++i, --n) padding.f[4U - extra + i] = pfloats[i];
-
-				ns_pv::XMStoreUByteN4(&out, padding.v);
-
-				memcpy(u8, reinterpret_cast<unsigned char *>(&out) + 4U - extra, extra);
-
-				u8 += extra;
-			}
-
-			// Blast through the aligned region.
-			auto from = reinterpret_cast<const ns_f2u8::XMVECTOR *>(ptr);
-
-			for (; n >= 4U; u8 += 4U, n-= 4U) ns_pv::XMStoreUByteN4(reinterpret_cast<ns_pv::XMUBYTEN4 *>(u8), *from++);
-
-			// Peel off any trailing floats.
-			if (n)
-			{
-				ns_f2u8::XMVECTORF32 padding = { 0 };
-
-				memcpy(padding.f, from, n * sizeof(float));
-
-				ns_pv::XMStoreUByteN4(&out, padding.v);
-
-				memcpy(u8, &out, n);
-			}
+			u8 += extra;
 		}
-	#endif
 
-	#ifdef __ANDROID__
-		template<> static void AuxFloatsToUnorm8s<true> (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool)
+		// Blast through the aligned region.
+		auto from = reinterpret_cast<const ns_f2u8::XMVECTOR *>(ptr);
+
+		for (; n >= 4U; u8 += 4U, n-= 4U) ns_pv::XMStoreUByteN4(reinterpret_cast<ns_pv::XMUBYTEN4 *>(u8), *from++);
+
+		// Peel off any trailing floats.
+		if (n)
 		{
-			if (CanUseNeon()) AuxFloatsToUnorm8s<false>(pfloats, u8, n, true);
+			ns_f2u8::XMVECTORF32 padding = { 0 };
 
-			else for (size_t i = 0; i < n; ++i) u8[i] = (unsigned char)(pfloats[i] * 255.0f);
+			memcpy(padding.f, from, n * sizeof(float));
+
+			ns_pv::XMStoreUByteN4(&out, padding.v);
+
+			memcpy(u8, &out, n);
 		}
-	#endif
+	}
+
+	template<> static void AuxFloatsToUnorm8s<true> (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool)
+	{
+		if (CanUseNeon()) AuxFloatsToUnorm8s<false>(pfloats, u8, n, true);
+
+		else for (size_t i = 0; i < n; ++i) u8[i] = (unsigned char)(pfloats[i] * 255.0f);
+	}
+#endif
 
 	void FloatsToUnorm8s (const float * _RESTRICT pfloats, unsigned char * _RESTRICT u8, size_t n, bool bNoTile)
 	{
 		AuxFloatsToUnorm8s(pfloats, u8, n, bNoTile);
 	}
 
-	#ifdef HAS_ACCELERATE
-		template<bool dummy = false> static void AuxUnorm8sToFloats (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool bNoTile)
-		{
-			vImage_Buffer src, dst;
+#ifdef HAS_ACCELERATE
+	template<bool dummy = false> static void AuxUnorm8sToFloats (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool bNoTile)
+	{
+		vImage_Buffer src, dst;
                 
-			src.data = (void *)u8;
-			dst.data = (void *)pfloats;
-			src.width = dst.width = src.rowBytes = n;
-			src.height = dst.height = 1;
-			dst.rowBytes = n * sizeof(float);
+		src.data = (void *)u8;
+		dst.data = (void *)pfloats;
+		src.width = dst.width = src.rowBytes = n;
+		src.height = dst.height = 1;
+		dst.rowBytes = n * sizeof(float);
                 
-			vImageConvert_Planar8toPlanarF(&src, &dst, 1.0f, 0.0f, bNoTile ? kvImageDoNotTile : 0);
-		}
-	#else
-		template<bool is_android = PlatformXS::is_android> static void AuxUnorm8sToFloats (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool)
+		vImageConvert_Planar8toPlanarF(&src, &dst, 1.0f, 0.0f, bNoTile ? kvImageDoNotTile : 0);
+	}
+#else
+	template<bool is_android = PlatformXS::is_android> static void AuxUnorm8sToFloats (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool)
+	{
+		void * ptr = pfloats;
+
+		MemoryXS::Align(16U, n * sizeof(float), ptr);
+
+		// Peel off any leading floats.
+		if (pfloats != ptr)
 		{
-			void * ptr = pfloats;
+			size_t extra = static_cast<float *>(ptr) - pfloats;
+			uint8_t padding[4] = { 0 };
 
-			MemoryXS::Align(16U, n * sizeof(float), ptr);
-
-			// Peel off any leading floats.
-			if (pfloats != ptr)
-			{
-				size_t extra = static_cast<float *>(ptr) - pfloats;
-				uint8_t padding[4] = { 0 };
-
-				for (size_t i = 0; i < extra; ++i, --n) padding[4U - extra + i] = *u8++;
+			for (size_t i = 0; i < extra; ++i, --n) padding[4U - extra + i] = *u8++;
 			
-				ns_pv::XMUBYTEN4 bytes{padding};
+			ns_pv::XMUBYTEN4 bytes{padding};
 
-				auto result = ns_pv::XMLoadUByteN4(&bytes);
+			auto result = ns_pv::XMLoadUByteN4(&bytes);
 
-				memcpy(pfloats - extra, reinterpret_cast<float *>(&result) + 4U - extra, extra * sizeof(float));
-			}
-
-			// Blast through the aligned region.
-			auto to = reinterpret_cast<ns_f2u8::XMVECTOR *>(ptr);
-
-			for (; n >= 4U; u8 += 4U, n-= 4U)
-			{
-				ns_pv::XMUBYTEN4 bytes{u8};
-
-				*to++ = ns_pv::XMLoadUByteN4(&bytes);
-			}
-
-			// Peel off any trailing floats.
-			if (n)
-			{
-				uint8_t padding[4] = { 0 };
-
-				for (size_t i = 0; i < n; ++i) padding[i] = u8[i];
-
-				ns_pv::XMUBYTEN4 bytes{padding};
-
-				auto result = ns_pv::XMLoadUByteN4(&bytes);
-
-				memcpy(to, &result, n * sizeof(float));
-			}
+			memcpy(pfloats - extra, reinterpret_cast<float *>(&result) + 4U - extra, extra * sizeof(float));
 		}
-	#endif
 
-	#ifdef __ANDROID__
-		template<> static void AuxUnorm8sToFloats<true> (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool)
+		// Blast through the aligned region.
+		auto to = reinterpret_cast<ns_f2u8::XMVECTOR *>(ptr);
+
+		for (; n >= 4U; u8 += 4U, n-= 4U)
 		{
-			if (CanUseNeon()) AuxUnorm8sToFloats<false>(u8, pfloats, n, true);
+			ns_pv::XMUBYTEN4 bytes{u8};
 
-			else for (size_t i = 0; i < n; ++i) pfloats[i] = float(u8[i]) / 255.0f;
+			*to++ = ns_pv::XMLoadUByteN4(&bytes);
 		}
-	#endif
+
+		// Peel off any trailing floats.
+		if (n)
+		{
+			uint8_t padding[4] = { 0 };
+
+			for (size_t i = 0; i < n; ++i) padding[i] = u8[i];
+
+			ns_pv::XMUBYTEN4 bytes{padding};
+
+			auto result = ns_pv::XMLoadUByteN4(&bytes);
+
+			memcpy(to, &result, n * sizeof(float));
+		}
+	}
+
+	template<> static void AuxUnorm8sToFloats<true> (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool)
+	{
+		if (CanUseNeon()) AuxUnorm8sToFloats<false>(u8, pfloats, n, true);
+
+		else for (size_t i = 0; i < n; ++i) pfloats[i] = float(u8[i]) / 255.0f;
+	}
+#endif
 
 	void Unorm8sToFloats (const unsigned char * _RESTRICT u8, float * _RESTRICT pfloats, size_t n, bool bNoTile)
 	{
